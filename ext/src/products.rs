6f59176c85fdca056878077706aeaaa9ff69c5b7
Joey Beauvais-Feisthauer <joeybf@wayne.edu>
2022-11-26 18:08:22 -0500

    Add cache for products
    This also stops us from accidentally writing to the same file twice and
    crashing, like previously. Might even sidestep the need for databases
    for the time being
    
    (cherry picked from commit 1b5f62ce2e380e9d7101498f021fe109c8b9d035)

--- ext/src/products.rs
+++ ext/src/products.rs
@@ -1,12 +1,12 @@
 use std::sync::Arc;
 
-use algebra::module::Module;
+use algebra::{module::Module, MuAlgebra};
 use dashmap::DashMap;
 use fp::{matrix::Matrix, vector::FpVector};
 
 use crate::{
-    chain_complex::{AugmentedChainComplex, BoundedChainComplex, ChainComplex},
-    resolution_homomorphism::ResolutionHomomorphism,
+    chain_complex::{AugmentedChainComplex, BoundedChainComplex, ChainComplex, FreeChainComplex},
+    resolution_homomorphism::MuResolutionHomomorphism,
     utils::QueryModuleResolution,
 };
 
@@ -19,6 +19,7 @@ pub type BidegreeGenerator = (u32, i32, usize);
 
 pub struct ProductStructure {
     resolution: Arc<QueryModuleResolution>,
+    cache: MuResolutionHomomorphismCache<false, QueryModuleResolution, QueryModuleResolution>,
     multiplication_table: DashMap<(BidegreeGenerator, Bidegree), Matrix>,
 }
 
@@ -28,8 +29,14 @@ impl ProductStructure {
             resolution.target().max_s() == 1 && resolution.target().module(0).is_unit(),
             "Product structure not supported for non-unit resolution"
         );
+        let cache = MuResolutionHomomorphismCache {
+            source: Arc::clone(&resolution),
+            target: Arc::clone(&resolution),
+            homs: DashMap::new(),
+        };
         Self {
             resolution,
+            cache,
             multiplication_table: DashMap::new(),
         }
     }
@@ -64,16 +71,9 @@ impl ProductStructure {
                 class[x_idx] = 1;
                 class
             };
-            let x_hom = ResolutionHomomorphism::from_class(
-                format!("({x_s},{x_t},{x_idx})"),
-                self.resolution(),
-                self.resolution(),
-                x_s,
-                x_t,
-                &x_class,
-            );
-            // Overkill. We could get away with just computing partially, depending on `y`.
-            x_hom.extend_all();
+            let x_hom = self
+                .cache
+                .from_class(format!("({x_s},{x_t},{x_idx})"), x_s, x_t, &x_class);
             let matrix_vec = x_hom.get_map(tot_s).hom_k(y_t);
             let matrix = Matrix::from_vec(self.resolution.prime(), &matrix_vec);
             let result_vec = matrix.row(y_idx).to_owned();
@@ -148,5 +148,63 @@ impl ProductStructure {
                 }
             }
         }
+    }
+}
+
+pub struct MuResolutionHomomorphismCache<const U: bool, CC1, CC2>
+where
+    CC1: FreeChainComplex<U>,
+    CC1::Algebra: MuAlgebra<U>,
+    CC2: AugmentedChainComplex<Algebra = CC1::Algebra>,
+{
+    source: Arc<CC1>,
+    target: Arc<CC2>,
+    homs: DashMap<(String, u32, i32, Vec<u32>), Arc<MuResolutionHomomorphism<U, CC1, CC2>>>,
+}
+
+impl<const U: bool, CC1, CC2> MuResolutionHomomorphismCache<U, CC1, CC2>
+where
+    CC1: FreeChainComplex<U>,
+    CC1::Algebra: MuAlgebra<U>,
+    CC2: AugmentedChainComplex<Algebra = CC1::Algebra>,
+{
+    pub fn from_class(
+        &self,
+        name: String,
+        shift_s: u32,
+        shift_t: i32,
+        class: &[u32],
+    ) -> Arc<MuResolutionHomomorphism<U, CC1, CC2>> {
+        let x = self
+            .homs
+            .entry((name.clone(), shift_s, shift_t, class.into()))
+            .or_insert_with(|| {
+                let result = MuResolutionHomomorphism::new_with_save(
+                    name,
+                    Arc::clone(&self.source),
+                    Arc::clone(&self.target),
+                    shift_s,
+                    shift_t,
+                    false,
+                );
+
+                let num_gens = result
+                    .source
+                    .module(shift_s)
+                    .number_of_gens_in_degree(shift_t);
+                assert_eq!(num_gens, class.len());
+
+                let mut matrix = Matrix::new(result.source.prime(), num_gens, 1);
+                for (k, &v) in class.iter().enumerate() {
+                    matrix[k].set_entry(0, v);
+                }
+
+                result.extend_step(shift_s, shift_t, Some(&matrix));
+                result.extend_all();
+                Arc::new(result)
+            })
+            .downgrade();
+        let y = &*x;
+        Arc::clone(y)
     }
 }
