ffdb304de5a5fa83f3c6c99265c6d3aefec479db
Joey Beauvais-Feisthauer <joeybf@wayne.edu>
2025-11-12 13:58:05 -0500

    Add `Display` impl for `FreeModuleHomomorphism`

--- ext/examples/differentials.rs
+++ ext/examples/differentials.rs
@@ -1,22 +1,15 @@
 //! This prints all the differentials in the resolution.
 
-use ext::{
-    chain_complex::{ChainComplex, FreeChainComplex},
-    utils::query_module,
-};
-use sseq::coordinates::BidegreeGenerator;
+use ext::{chain_complex::ChainComplex, utils::query_module};
 
 fn main() -> anyhow::Result<()> {
     ext::utils::init_logging()?;
 
     let resolution = query_module(None, false)?;
 
-    for b in resolution.iter_stem() {
-        for i in 0..resolution.number_of_gens_in_bidegree(b) {
-            let gen = BidegreeGenerator::new(b, i);
-            let boundary = resolution.boundary_string(gen);
-            println!("d x_{gen:#} = {boundary}");
-        }
+    for s in 0..resolution.next_homological_degree() {
+        println!("{}", resolution.differential(s));
     }
+
     Ok(())
 }
